dfc6df08bb22b21d0cb95a674230382824d95508
Marc-André Legault <legaultmarc@gmail.com>
2015-06-16 16:56:35 -0400

    Refactored the sample order validation for phenotype databases.

--- forward/phenotype/db.py
+++ forward/phenotype/db.py
@@ -33,13 +33,47 @@ class PhenotypeDatabaseInterface(object):
     def get_phenotype_vector(self, name):
         raise NotImplementedError()
 
-    def set_sample_order(self, sequence):
+    def set_sample_order(self, sequence, allow_subset=False):
         """Set the order of the samples."""
         raise NotImplementedError()
 
     def get_sample_order(self):
         """Get the order of the samples."""
         raise NotImplementedError()
+
+    @staticmethod
+    def validate_sample_sequences(old_seq, new_seq, allow_subset):
+        """Compares sample sequences to validate the new sequence order.
+
+        This can optionally be used by subclasses when writing the
+        `set_sample_order` method. We recommend using to properly log relevant
+        information.
+
+        """
+        # Make sure that all the elements are in the provided sequence.
+        old_seq = set(old_seq)
+        new_seq = set(new_seq)
+
+        missing = old_seq - new_seq
+        extra = new_seq - old_seq
+
+        if not allow_subset and missing:
+            message = ("Can't set the sequence because some entries are "
+                       "missing resulting in ambiguous order.")
+            raise ValueError(message)
+
+        elif missing:
+            message = ("Some samples were discarded when reordering "
+                       "phenotype information ({} samples discarded). This "
+                       "could be because no genotype information is "
+                       "available for these samples.")
+            message = message.format(len(missing))
+            logger.warning(message)
+
+        if extra:
+            extra = ", ".join(extra)
+            raise ValueError("Some of the given samples are not in the "
+                             "phenotype database ({}).".format(extra))
 
     def get_correlation_matrix(self, names):
         """Get a correlation matrix for the specified names.
@@ -77,42 +111,24 @@ class ExcelPhenotypeDatabase(PhenotypeDatabaseInterface):
         return self.data.columns
 
     def set_sample_order(self, sequence, allow_subset=False):
-        # Make sure that all the elements are in the provided sequence.
-        set_phen = set(self.data.index.values)
-        set_given = set(sequence)
 
-        missing = set_phen - set_given
-        extra = set_given - set_phen
-
-        if not allow_subset and missing:
-            message = ("Can't set the sequence because some entries are "
-                       "missing resulting in ambiguous order.")
-            raise ValueError(message)
-
-        elif missing:
-            message = ("Some samples were discarded when reordering "
-                       "phenotype information ({} samples discarded). This "
-                       "could be because no genotype information is "
-                       "available for these samples.")
-            message = message.format(len(missing))
-            logger.warning(message)
-
-        if extra:
-            extra = ", ".join(extra)
-            raise ValueError("Some of the given samples are not in the "
-                             "phenotype database ({}).".format(extra))
+        ExcelPhenotypeDatabase.validate_sample_sequences(
+            self.get_sample_order(warn=False),
+            sequence,
+            allow_subset
+        )
 
         self.data = self.data.loc[sequence, :]
         self._order_is_set = True
 
-    def get_sample_order(self):
-        if not self._order_is_set:
+    def get_sample_order(self, warn=True):
+        if not self._order_is_set and warn:
             logger.warning("No sample order was given for the phenotype "
                            "database.")
         return self.data.index.values
 
-    def get_phenotype_vector(self, name):
-        if not self._order_is_set:
+    def get_phenotype_vector(self, name, warn=True):
+        if not self._order_is_set and warn:
             logger.warning("The order of samples for the database has not "
                            "been set. Make sure that it is consistent with "
                            "the genetic database (consistent order).")
