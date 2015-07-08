Fixed a (stupid) bug in the maf computation.

forward/tests/dummies.py
@@ -42,6 +42,7 @@ class DummyPhenDatabase(PhenotypeDatabaseInterface):
             "var3": np.random.binomial(1, 0.1, 100),  # discrete
             "var4": np.random.binomial(1, 0.3, 100),  # discrete
             "var5": np.random.gamma(2, 2, 100),  # continous
+            "var6": np.random.binomial(1, 0.8, 100),  # discrete
         }
 
         # Insert some missings.
@@ -130,7 +131,7 @@ class DummyGenotypeDatabase(GenotypeDatabaseInterface):
             # Use the probability of being homo minor given that the sample
             # carries the mutation.
             p = maf ** 2
-            p /= p + (2 * maf * (1 - maf))
+            p /= p + 2 * maf * (1 - maf)
             mutants = self.genotypes[snp] == 1
             n_mutants = np.sum(mutants)
 

forward/tests/test_interfaces.py
@@ -221,7 +221,7 @@ class TestGenoDBInterface(object):
             # Hybrids
             self.assertEquals(
                 var.maf,
-                np.nansum(geno) / np.sum(~np.isnan(geno))
+                np.nansum(geno) / (2 * np.sum(~np.isnan(geno)))
             )
             self.assertEquals(
                 var.completion_rate,
@@ -420,7 +420,7 @@ class TestDummyPhenotypeDatabase(TestPhenDBInterface, unittest.TestCase):
     def setUp(self):
         super(TestDummyPhenotypeDatabase, self).setUp()
         self.db = dummies.DummyPhenDatabase()
-        self._variables = ["var1", "var2", "var3", "var4", "var5"]
+        self._variables = ["var1", "var2", "var3", "var4", "var5", "var6"]
 
 
 class TestDummyGenotypeDatabase(TestGenoDBInterface, unittest.TestCase):